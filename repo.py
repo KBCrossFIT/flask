ba3f939c740e944725f4daecd28c85638a8353a4
wxxhyeong <wxxhyeong@gmail.com>
2024-10-07 17:03:30 +0900

    3_

---
+++ repo.py
@@ -0,0 +1,25 @@
+from youtube_transcript import get_video_transcript
+
+class persona_repo:
+
+    personas = {}
+
+    # 스크립트 추출하여 저장
+    def script_insert(self,name, url):
+
+        self.set_persona_name(name)
+        self.personas[name]["youtube_scripts"].append("1." + get_video_transcript(url))
+
+
+    # 페르소나 db에 키 값 생성
+    def set_persona_name(self,name):
+
+        if name in self.personas:
+            return
+    
+        self.personas[name] = {
+            'name' : name,
+            'youtube_scripts' : []
+        }
+
+    
---
+++ youtube_transcript.py
@@ -0,0 +1,27 @@
+from youtube_transcript_api import YouTubeTranscriptApi
+
+def get_video_transcript(video_url):
+    script = ""
+
+    video_id = get_video_id(video_url)
+    try:
+        # 자막 가져오기 (지원되는 자막 언어 확인)
+        transcript = YouTubeTranscriptApi.get_transcript(video_id)
+        
+        # 자막 내용 출력
+        for entry in transcript:
+            script += entry['text']
+            #print(f"{entry['text']}")
+    
+    except Exception as e:
+        print(f"자막을 가져오는 중 오류 발생: {e}")
+
+    return script
+
+# 유튜브 비디오 ID로 자막을 가져옵니다.
+def get_video_id(video_url):
+    video_id = video_url.split('v=')[1][:11]
+    return video_id
+
+# video_id = get_video_id("")  # 여기에 실제 비디오 ID 입력
+get_video_transcript("https://www.youtube.com/watch?v=kWY9mNMz4iI")
