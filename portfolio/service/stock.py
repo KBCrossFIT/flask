93c53e5e7223dfec6260f96871a34d303eb69ea0
김민철(MInCheol Kim) <ofin0613@naver.com>
2024-10-10 06:50:58 +0900

    refactor: 함수명 & 구조 수정

--- portfolio/service/stock.py
+++ portfolio/service/stock.py
@@ -1,30 +1,61 @@
-import pandas as pd
+import numpy as np
 import FinanceDataReader as fdr
 
 from pypfopt.expected_returns import mean_historical_return
-from pypfopt.risk_models import CovarianceShrinkage
-from pypfopt.efficient_frontier import EfficientFrontier
+from datetime import datetime, timedelta
+from scipy.stats import norm
 
-stock_list = [
-    ["삼성전자","005930"],
-    ["SK하이닉스", "000660"],
-    ["기아", "000270"],
-    ["카카오", "035720"],
-    ["KB금융그룹", "105560"]
-]
-df_list=[fdr.DataReader(code[1], '2023', '2024')['Close'] for code in stock_list]
-df = pd.concat(df_list, axis=1)
-df.columns = [name for name, code in stock_list]
-df.head(10)
+# stock = {
+#     "stockCode": '035720',  # 주식코드
+#     "amount": 10  # 주식 수
+# }
 
-mu = mean_historical_return(df)
-S = CovarianceShrinkage(df).ledoit_wolf()
+def calculate_stock(stock):
+    calc_data = {
+        'expectedReturn': 0,
+        'riskLevel': 0,
+        'amount': 0
+    }
 
-ef = EfficientFrontier(mu, S)
-weights = ef.max_sharpe()
-print(weights)
-cleaned_weights = ef.clean_weights()
-print(cleaned_weights)
+    ticker = stock['stockCode']
+    end_date = (datetime.now() - timedelta(days=2)).strftime('%Y-%m-%d')
+    start_date = (datetime.now() - timedelta(days=1100)).strftime('%Y-%m-%d')
 
-performance = ef.portfolio_performance()
-print(performance)
+    # 주식 데이터 가져오기
+    df = fdr.DataReader(ticker, start_date, end_date)
+
+    # 주식 가격이 존재하는지 확인
+    if df.empty:
+        raise ValueError("주식 데이터가 없습니다.")
+
+    # 기대수익률 계산
+    expected_return = mean_historical_return(df['Close'].dropna()) * 100
+    calc_data['expectedReturn'] = round(expected_return[0], 2)
+
+    # 수익률 변동성 계산
+    std = np.std(df['Change'].dropna()) #표준편차 계산
+    volatility = std * np.sqrt(252) * 100
+
+    if(volatility > 25):
+        calc_data['riskLevel'] = 1
+    elif(volatility > 15):
+        calc_data['riskLevel'] = 2
+    elif(volatility > 10):
+        calc_data['riskLevel'] = 3
+    elif(volatility > 5):
+        calc_data['riskLevel'] = 4
+    elif(volatility > 0.5):
+        calc_data['riskLevel'] = 5
+    else:
+        calc_data['riskLevel'] = 6
+
+    calc_data['amount'] = stock['amount'] * df.loc[end_date, 'Close']
+
+    # 샤프 비율 계산 (주석 처리된 부분)
+    risk_free_rate = 0.01  # 무위험 이자율 (예: 1%)
+    sharpe_ratio = (expected_return - risk_free_rate) / volatility
+
+    return calc_data
+
+# 계산 결과 출력
+# print(calculate_stock(stock))
