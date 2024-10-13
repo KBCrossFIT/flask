5171c5cc8262609fd59f743db5615102edf535aa
김민철(MInCheol Kim) <ofin0613@naver.com>
2024-10-13 15:42:04 +0900

    fix: 적금 계산시, 월 납입액/총 투자금액 기준 변경

--- portfolio/service/saving.py
+++ portfolio/service/saving.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
 # saving = {
-#     'rsrvType': 'F', # 예금/적금 구분
-#     'saveTerm': 12, # 만기
+#     'rsrvType': 'S', # 예금/적금 구분
+#     'saveTerm': 6, # 만기
 #     'intrType': "복리", # 단리/복리
-#     'intrRate': 3.50, # 금리(최저, 최대는 프론트에서 선택)
+#     'intrRate': 4.75, # 금리(최저, 최대는 프론트에서 선택)
 #     'amount': 1000000, # 예금의 경우 총액, 적금의 경우 월 납입액
 # }
 
@@ -15,21 +15,26 @@ def calculate_saving(saving):
         'amount': 0
     }
     
-    calc_data['amount'] = saving['amount']
-    
-    if saving['intrType'] == '단리':
-        expected_return = saving['intrRate'] * (saving['saveTerm'] / 12)
-    else:
-        if saving['rsrvType'] == '':  # 예금
+    if saving['rsrvType'] == '':    # 예금
+        calc_data['amount'] = saving['amount']
+        if saving['intrType'] == '단리':
+            expected_return = saving['intrRate'] * (saving['saveTerm'] / 12)
+        else:
             expected_return = ((1 + saving['intrRate'] / 100 / 12) ** (saving['saveTerm']) - 1) * 100
-        else:  # 적금
-            total_amount = 0
+            
+    else: # 적금
+        calc_data['amount'] = saving['amount'] * saving['saveTerm']
+        total_amount = 0
+        if saving['intrType'] == '단리': 
+            total_amount = calc_data['amount'] + (saving['amount'] * ((saving['saveTerm'] * (saving['saveTerm'] + 1)) / 2) * saving['intrRate'] / 100 / 12)
+            expected_return = ((total_amount / (calc_data['amount'])) - 1) * 100 
+        else: 
             for i in range(saving['saveTerm']):
                 total_amount += saving['amount'] * (1 + saving['intrRate'] / 100 / 12) ** ((saving['saveTerm']) - i)
-                expected_return = (total_amount - (saving['amount'] * saving['saveTerm'])) / (saving['amount'] * saving['saveTerm']) * 100
-                
+            expected_return = (total_amount - (saving['amount'] * saving['saveTerm'])) / (saving['amount'] * saving['saveTerm']) * 100   
+
     calc_data['expectedReturn'] = round(expected_return, 2)
     
     return calc_data
-    
+
 # print(calculate_saving(saving))
